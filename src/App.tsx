e60b9ca7dd858169b8aa761849324f27ace10c09
McCallWang <13567272790@163.com>
2020-11-10 11:25:41 +0800

    安装依赖 react-router，页面路由基本结构完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, NavLink, Route, Redirect} from 'react-router-dom';
+import Money from './components/Money';
+import Details from './components/Details';
+import Statistics from './components/Statistics';
 
 function App() {
   return (
     <div>
-      Hello React!
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/details" exact>明细页</NavLink>
+            </li>
+            <li>
+              <NavLink to="/money">记账页</NavLink>
+            </li>
+            <li>
+              <NavLink to="/statistics">统计页</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <Switch>
+            <Redirect from='/' to='details' exact/>
+            <Route path="/details">
+              <Details/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+          </Switch>
+        </main>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Details.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Details = () => {
+  return (<div>明细页</div>);
+};
+
+export default Details;
