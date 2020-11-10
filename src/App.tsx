613b0f7f61bf51338c85b56c8bba8fbda39f0fcc
McCallWang <13567272790@163.com>
2020-11-10 14:15:32 +0800

    用 Flex 来定位导航栏

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,38 @@ import Money from './components/Money';
 import Details from './components/Details';
 import Statistics from './components/Statistics';
 import NotMatch from './components/NotMatch';
+import styled from 'styled-components';
+
+
+const StyledWrapper = styled.div`
+  margin: 0 auto;
+  height: 100vh;
+  max-width: 500px;
+  display:flex;
+  flex-direction: column;
+`;
+const StyledMain = styled.main`
+  border:1px solid blue;
+  flex-grow: 1;
+`;
+const StyledNav = styled.nav`
+  border: 1px solid green;
+  > ul{
+      display:flex;
+      flex-direction: row;
+    > li{
+      width: 33.333%;
+      text-align:center;
+      padding: 16px;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div>
+    <StyledWrapper>
       <Router>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/details" exact>明细页</NavLink>
-            </li>
-            <li>
-              <NavLink to="/money">记账页</NavLink>
-            </li>
-            <li>
-              <NavLink to="/statistics">统计页</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <main>
+        <StyledMain>
           <Switch>
             <Redirect from='/' to='details' exact/>
             <Route path="/details">
@@ -38,9 +51,22 @@ function App() {
               <NotMatch/>
             </Route>
           </Switch>
-        </main>
+        </StyledMain>
+        <StyledNav>
+          <ul>
+            <li>
+              <NavLink to="/details" exact>明细页</NavLink>
+            </li>
+            <li>
+              <NavLink to="/money">记账页</NavLink>
+            </li>
+            <li>
+              <NavLink to="/statistics">统计页</NavLink>
+            </li>
+          </ul>
+        </StyledNav>
       </Router>
-    </div>
+    </StyledWrapper>
   );
 }
 
