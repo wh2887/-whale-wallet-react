682b2767ba486fa67c3428af415bf5f12ceccc6c
McCallWang <13567272790@163.com>
2020-11-10 10:41:53 +0800

    删除不必要的文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import './App.css';
-import styled from 'styled-components';
-
-const StyledButton = styled.button`
-  color: red;
-`;
 
 function App() {
   return (
     <div>
-      <StyledButton>按钮</StyledButton>
+      Hello React!
     </div>
   );
 }
