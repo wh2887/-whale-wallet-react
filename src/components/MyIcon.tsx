152b39493441a325b7476dd66066f06cbd65fe3f
McCallWang <13567272790@163.com>
2020-11-10 17:08:08 +0800

    解决 MyIcon 组件图标文件的多次引用。直接引入图标文件夹。

--- src/components/MyIcon.tsx
+++ src/components/MyIcon.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-require('../icons/money.svg');
-require('../icons/details.svg');
-require('../icons/statistics.svg');
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('../icons', true, /\.svg$/));} catch (error) {console.log(error);}
 
 type Props = {
   name: string
